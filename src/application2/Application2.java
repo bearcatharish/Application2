048889932945a457c8e8bb3e9ead0157ea18390e
S525810 <S525810@NW31603.nwmissouri.edu>
2016-09-08 16:50:32 -0500

    changed the first sysout in commit

--- src/application2/Application2.java
+++ src/application2/Application2.java
@@ -17,7 +17,7 @@ public class Application2 {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("Testing Git Application 2");
+        System.out.println("Testing branch in Git Application 2");
         System.out.println("Adding 2nd line");
         System.out.println("Check the differences");
         System.out.println("Difference2");
